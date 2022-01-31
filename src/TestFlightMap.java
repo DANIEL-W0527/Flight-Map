f13778a949b9edd1762ce1816aecd05bf957e295
Jinghao Wang <jinghao@usc.edu>
2022-01-31 11:53:34 -0800

    test and build file
    build.xml and test

---
+++ src/TestFlightMap.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestFlightMap{
+    FlightMap fm;
+    @Before
+    /** Set up method to create the test fixture*/
+    public void initialize(){
+
+    }
+}
