c1561e4bf906fa8851af064c45d55334e4008df8
Jinghao Wang <jinghao@usc.edu>
2022-01-31 19:02:02 -0800

    Comments in files

--- src/FlightMap.java
+++ src/FlightMap.java
@@ -10,14 +10,18 @@ public class FlightMap{
     public String inputFileName;
     public String outputFileName;
 
+    //constructor
     public FlightMap(String input, String output){
+        // generate two maps to store the flight schedules and its cost
         routeMap = new TreeMap<String,LinkedList<String>>();
         costMap = new TreeMap<String,Integer>();
         citiesSet = new TreeSet<String>();
+        // store the input and output file name
         inputFileName = input;
         outputFileName = output;
     }
 
+    // store the flight schedule into two maps and a set(used to keep track which cities could be reached later)
     public void readInputFile(){
         try {
             File inputFile = new File(inputFileName);
@@ -26,55 +30,58 @@ public class FlightMap{
             while (in.hasNextLine()){
                 String line = in.nextLine();
                 String[] splitted = line.split(" ");
+                // if citiesSet does not contain the departing city, add to the citiesSet
+                // create adjacency list for that city with arriving city
                 if (!citiesSet.contains(splitted[0])){
                     citiesSet.add(splitted[0]);
                     LinkedList<String> newll = new LinkedList<String>();
                     newll.add(splitted[1]);
                     routeMap.put(splitted[0],newll);
                 }
+                // if the departing city is in the routeMap, add arriving city into its adjacency list in routeMap
                 else{
                     routeMap.get(splitted[0]).add(splitted[1]);
                 }
+                // if the arriving city is not in the citiesSet, add it to the routeMap
                 if (!citiesSet.contains(splitted[1])){
                     citiesSet.add(splitted[1]);
                     LinkedList<String> newll = new LinkedList<String>();
                     routeMap.put(splitted[1],newll);
                 }
                 String routeName = splitted[0] + splitted[1];
+                // record the cost for each flight in the costMap
                 costMap.put(routeName, Integer.parseInt(splitted[2]));
             }
+            // close the scanner
             in.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
-
-    public void search(String route, String city, Integer cost,PrintWriter newPW){
+    // use recursion to get the route for reachable cities and print it in the output file
+    public void search(String route, String city, Integer cost, PrintWriter newPW){
+        // Iterate through all possible cities city could reach to
         ListIterator listIt = routeMap.get(city).listIterator();
         while (listIt.hasNext()){
             String tempRoute = route;
             Integer tempCost = cost;
             String temp = (String) listIt.next();
+            // citiesSet are all cities that we haven't explored that if city could reach to
             if (citiesSet.contains(temp)){
                 tempRoute += ", " + temp;
                 tempCost += costMap.get(city+temp);
+                // if reached by city, remove it from citiesSet
                 citiesSet.remove(temp);
                 newPW.println(String.format("%-15s %-25s %-10s", temp, tempRoute, tempCost));
             }
+            // if this city is not in the citiesSet, we return because we already find one route could reach to city before
             else{
                 return;
             }
+            // recursion to search for next levels of cities
             search(tempRoute, temp, tempCost,newPW);
         }
     }
     public static void main(String args[]){
-        /**
-        FlightMap fm = new FlightMap("inputfile2.txt","outputfile.txt");
-        fm.readInputFile();
-        System.out.println(fm.routeMap);
-        System.out.println(fm.costMap);
-        System.out.println(fm.citiesSet);
-        System.out.println(fm.originCity);
-        */
     }
 }
--- src/SearchMap.java
+++ src/SearchMap.java
@@ -4,9 +4,12 @@ import java.io.*;
 public class SearchMap{
 
     public static void main(String args[]) {
+        // get user's input for input file and output file name from command
         FlightMap fm = new FlightMap(args[0],args[1]);
+        // load input file and store the map
         fm.readInputFile();
         try {
+            // print the table to the output file with all reachable cities
             PrintWriter output = new PrintWriter(args[1]);
             output.println(String.format("%-15s %-25s %-10s", "Destination", "Flight Route from " + fm.originCity, "Total Cost"));
             fm.search(fm.originCity, fm.originCity, 0, output);
